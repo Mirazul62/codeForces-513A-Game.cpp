03cd8002c27490ecdcf7c2a906748f5e1839e0a3
Mirazul Islam <mirazkuetcse62@gmail.com>
2018-06-15 16:25:04 +0600

    513A	Game solve in c++

---
+++ Game.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n,m,a,b;
+    cin>>n>>m>>a>>b;
+    if(n>m)
+        cout<<"First";
+    else if(n<m)
+        cout<<"Second";
+    else
+        cout<<"Second";
+}
